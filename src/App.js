4ea77aca394214e6a336e3805a3cd7c2a446da0d
Ivonaldo Soares <vonaldojrsoares@gmail.com>
2020-10-08 22:55:14 -0300

    correcao

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import MovieList from './components/MovieList/MovieList';
 
--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -20,7 +20,6 @@ class MovieCard extends Component {
         const {
             Title,
             Released,
-            Genre,
             Plot,
             Poster,
             imdbRating
